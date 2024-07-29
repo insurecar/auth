f20935fe4430d858c3607ea2240e7eba2052d3f7
poldevy <rostyslavshyian@gmail.com>
2024-07-29 12:24:07 +0200

    finsished 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-red-700 text-4xl bg-yellow-500">
+    <h1 className="text-red-700 text-4xl bg-yellow-500 ">
       Welcome to the course
     </h1>
   );
